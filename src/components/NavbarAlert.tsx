fce1c0984fde0ad1684bbb87f9007e61c2f3513d
Giuseppe Vigneri <geppe1990@gmail.com>
2025-05-28 17:29:00 +0200

    NavbarAlert

--- src/components/NavbarAlert.tsx
+++ src/components/NavbarAlert.tsx
@@ -1,13 +1,31 @@
 import settings from "../../settings.ts"
+import { useState } from "react"
+import { MdClose } from "react-icons/md"
 
 const NavbarAlert = () => {
-	if (!(!settings.firstName && !settings.lastName)) {
+	const [opened, setOpened] = useState<boolean>(!settings.firstName && !settings.lastName)
+
+	const handleClose = () => {
+		setOpened(false)
+	}
+
+	if (!opened) {
 		return
 	}
+
 	return (
-		<div className={"bg-red-600 text-white text-left p-2 mb-4 text-sm"}>
-			<b>Attenzione</b>: se è la prima volta che usi questa applicazione, modifica i parametri
-			nel file settings.json
+		<div
+			className={
+				"bg-red-600 text-white text-left p-2 mb-4 text-sm flex justify-between items-center"
+			}
+		>
+			<div>
+				<b>🔥 Attenzione</b>: se è la prima volta che usi questa applicazione, modifica i
+				parametri nel file settings.json
+			</div>
+			<div className={"text-white cursor-pointer"} onClick={handleClose}>
+				<MdClose />
+			</div>
 		</div>
 	)
 }
