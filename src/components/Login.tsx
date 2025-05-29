9dbbfe65d1ebaf449039fe315b4f65efd8bd7d8d
Giuseppe Vigneri <geppe1990@gmail.com>
2025-05-29 22:37:50 +0200

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -11,8 +11,8 @@ const Login: React.FC<LoginProps> = ({ callback }) => {
 		<div className="flex flex-col items-center justify-center mt-20 text-center space-y-6">
 			<h1 className="text-4xl font-bold">Benvenuto!</h1>
 			<p className="text-lg text-gray-600 max-w-md">
-				Accedi con Google per generare automaticamente un proforma basata sui tuoi eventi
-				del calendario.
+				Accedi con Google per generare automaticamente un proforma basato sul tuo
+				calendario.
 			</p>
 			<LoginButton onClick={callback} />
 		</div>
