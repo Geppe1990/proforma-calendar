a772947759209104ae5566f91382cc2d1d837e25
Giuseppe Vigneri <geppe1990@gmail.com>
2025-05-29 22:06:38 +0200

    Loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,27 @@ import React from "react"
 
 const Loading: React.FC = () => {
 	return (
-		<div className="flex flex-col items-center justify-center mt-20 text-center space-y-6">
-			<p className="text-lg text-gray-600">Caricamento in corso...</p>
+		<div className="flex flex-col items-center justify-center text-center h-full absolute top-0 left-[50%]">
+			<svg
+				className="animate-spin h-8 w-8"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+			>
+				<circle
+					className="opacity-25 text-gray-400"
+					cx="12"
+					cy="12"
+					r="10"
+					stroke="currentColor"
+					strokeWidth="4"
+				/>
+				<path
+					className="opacity-75 text-blue-600"
+					fill="currentColor"
+					d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+				/>
+			</svg>
 		</div>
 	)
 }
