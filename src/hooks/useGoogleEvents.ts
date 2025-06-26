726c2733ac70ddcd9d28ac2af7564a81c5ef4bac
Giuseppe Vigneri <geppe1990@gmail.com>
2025-06-26 21:01:22 +0200

    Refactoring useGoogleEvents

--- src/hooks/useGoogleEvents.ts
+++ src/hooks/useGoogleEvents.ts
@@ -19,38 +19,37 @@ export function useGoogleEvents(token: string | null, year: number, month: numbe
 		localStorage.setItem(STORAGE_KEY, JSON.stringify(updated))
 	}
 
-	const fetchEvents = async (token: string, year: number, month: number) => {
-		const start = dayjs(`${year}-${String(month).padStart(2, "0")}-01`)
-			.startOf("month")
-			.toISOString()
-		const end = dayjs(`${year}-${String(month).padStart(2, "0")}-01`)
-			.endOf("month")
-			.toISOString()
-
-		try {
-			const res = await fetch(
-				`https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=${start}&timeMax=${end}&singleEvents=true&orderBy=startTime`,
-				{ headers: { Authorization: `Bearer ${token}` } }
-			)
-
-			if (res.status === 401) {
-				console.warn("Token non valido.")
-				localStorage.removeItem("google_token")
-				return setEvents([])
-			}
-
-			const data = await res.json()
-			const hiddenIds = getHiddenEventIds()
-			const visibleEvents = (data.items || []).filter(
-				(event: CalendarEvent) => !hiddenIds.includes(event.id)
-			)
-			setEvents(visibleEvents)
-		} catch (error) {
-			console.error("Errore nel recupero eventi:", error)
-		}
-	}
-
 	useEffect(() => {
+		const fetchEvents = async (token: string, year: number, month: number) => {
+			const start = dayjs(`${year}-${String(month).padStart(2, "0")}-01`)
+				.startOf("month")
+				.toISOString()
+			const end = dayjs(`${year}-${String(month).padStart(2, "0")}-01`)
+				.endOf("month")
+				.toISOString()
+
+			try {
+				const res = await fetch(
+					`https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=${start}&timeMax=${end}&singleEvents=true&orderBy=startTime`,
+					{ headers: { Authorization: `Bearer ${token}` } }
+				)
+
+				if (res.status === 401) {
+					console.warn("Token non valido.")
+					localStorage.removeItem("google_token")
+					return setEvents([])
+				}
+
+				const data = await res.json()
+				const hiddenIds = getHiddenEventIds()
+				const visibleEvents = (data.items || []).filter(
+					(event: CalendarEvent) => !hiddenIds.includes(event.id)
+				)
+				setEvents(visibleEvents)
+			} catch (error) {
+				console.error("Errore nel recupero eventi:", error)
+			}
+		}
 		if (token && year && month) {
 			fetchEvents(token, year, month).finally(() => setIsLoading(false))
 		} else {
